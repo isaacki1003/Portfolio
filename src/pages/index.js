fbd91c375a6b790c465235a212200d9a5dd9724f
hoon <isaacki1003@gmail.com>
2023-06-12 01:01:42 -0700

    update for google analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { Helmet } from 'react-helmet';
 import { Layout, Hero, About, Jobs, Featured, Projects, Contact } from '@components';
 
 const StyledMainContainer = styled.main`
@@ -9,6 +10,17 @@ const StyledMainContainer = styled.main`
 
 const IndexPage = ({ location }) => (
   <Layout location={location}>
+    <Helmet>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-7P7XR85LVX"></script>
+      <script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-7P7XR85LVX');
+        `}
+      </script>
+    </Helmet>
     <StyledMainContainer className="fillHeight">
       <Hero />
       <About />
@@ -23,5 +35,4 @@ const IndexPage = ({ location }) => (
 IndexPage.propTypes = {
   location: PropTypes.object.isRequired,
 };
-
 export default IndexPage;
